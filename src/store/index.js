2555edf8bcdccefcc4674f3934c3a9bf2ebedfb0
llycoder <llycoder@gmail.com>
2018-07-08 21:43:49 +0800

    finish

--- src/store/index.js
+++ src/store/index.js
@@ -100,7 +100,7 @@ const store = new Vuex.Store({
         loadMenuList: ({commit}) => {
             commit(types.LOAD_MENU, defaultValue.menuList)
         },
-        changeCurrentMenu: ({ state, commit }, {fullPath }) => {
+        changeCurrentMenu: ({ state, commit }, {fullPath}) => {
             const a = getCurrentMenu(fullPath, state.menuList);
             commit(types.LOAD_CURRENT_MENU, a.reverse());
         },
